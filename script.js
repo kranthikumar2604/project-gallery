064867b41114bdfe0e4dd923c7ede2169076d3e3
kranthikumar2604 <kranthimattepally1341@gmail.com>
2025-03-24 01:05:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const tasks = [
-    { title: "Task 1", url: "./task1.html" },
-    { title: "Task 2", url: "./task9.html" },
-    { title: "Task 3", url: "./task10.html" },
-    { title: "Task 4", url: "./task12.html" },
-    { title: "Task 5", url: "./task13.html" },
-    { title: "task 6", url: "./task2.html" },
-    { title: "task 7", url: "./task4.html" },
-    { title: "task 8", url: "./task5.html" },
-    { title: "task 9", url: "./task6.html" },
-    { title: "task 10", url: "./task8.html" },
-    { title: "task 11", url: "./task14.html" },
-    { title: "task 12", url: "./task15.html" },
-    { title: "task 13", url: "./task16.html"}
+    { title: "Task 1-hover", url: "./projects/task 1/task1.html" },
+    { title: "Task 2-sum", url: "./projects/task 7/task9.html" },
+    { title: "Task 3-student info", url: "./projects/task 8/task10.html" },
+    { title: "Task 4-random value", url: "./projects/task 9/task12.html" },
+    { title: "Task 5-creative sites", url: "./projects/task 10/task13.html" },
+    { title: "task 6-javascript", url: "./projects/task 2/task2.html" },
+    { title: "task 7-alert", url: "./projects/task 3/task4.html" },
+    { title: "task 8-calculator", url: "./projects/task 4/task5.html" },
+    { title: "task 9-string operations", url: "./projects/task 5/task6.html" },
+    { title: "task 10-grading calculator", url: "./projects/task 6/task8.html" },
+    { title: "task 11-age calculator", url: "./projects/task 11/task14.html" },
+    { title: "task 12-stack", url: "./projects/task 12/task15.html" },
+    { title: "task 13-dashboard", url: "./projects/task 13/task16.html"}
     
    
 ];
